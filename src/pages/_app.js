e4f55f9713f5e5a82f7cb61cc312a7141c33da02
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-11-12 19:58:54 +0100

    Move title to app file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from 'next/app';
+import Head from 'next/head';
 import '../styles/main.css';
 
 export default class Application extends App {
@@ -8,6 +9,9 @@ export default class Application extends App {
 
     return (
       <div className='flex flex-col flex-1 md:justify-center max-w-3xl mx-auto p-5 w-full'>
+        <Head>
+          <title>Personnummer</title>
+        </Head>
         <div>
           <h1 className='text-4xl md:text-6xl text-center text-gray-700'>personnummer</h1>
         </div>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -18,7 +18,6 @@ export default class AppDocument extends Document {
     return (
       <html lang='en'>
         <Head>
-          <title>Personnummer</title>
           <meta charSet='UTF-8' />
           <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
