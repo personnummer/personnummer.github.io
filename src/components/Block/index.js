38b2b68b7085b8b61eefa199c862ff97825c12f0
JiteBot <accounts+github@jitesoft.com>
2019-11-12 07:09:17 +0000

    Deploy(07561ca): Develop = main branch. Master => Deploy.
    Repo: personnummer/personnummer.github.io
    Workflow: .github/workflows/deploy.yml
    SHA: 07561caac44ce4629a57347536bae7d75f726240
    Ref: refs/heads/develop
    Actor: Johannestegner

--- src/components/Block/index.js
+++
@@ -1,6 +0,0 @@
-export default ({ children, title = '' }) => (
-  <div className='mt-5'>
-    {title && <h2 className='text-xl text-gray-700 border-b border-gray mb-2'>{title}</h2>}
-    {children}
-  </div>
-);
