3156dac03c0f102233adcb2e58bf6640ee17472e
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-11-12 20:02:50 +0100

    Add analytics

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,7 +9,15 @@ export default class AppDocument extends Document {
 
   setGoogleAnalytics () {
     return {
-      __html: ''
+      __html: `(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('set', 'anonymizeIP', true);
+        ga('create','UA-20991800-17','auto');
+        ga('require', 'linkid');
+        ga('send','pageview');`
     };
   }
 
