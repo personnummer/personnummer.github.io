13d34f3244e5c3f5d463483d3e0332067db16252
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-11-12 18:18:39 +0100

    Fix font size on mobile

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -1,6 +1,6 @@
 export default ({ border = true, children, className, title = '' }) => (
   <div className={`mt-5 ${className}`}>
-    {title && <h2 className={`text-xl text-gray-700 mb-2 ${border ? 'border-b border-gray' : ''}`}>{title}</h2>}
+    {title && <h2 className={`text-lg md:text-xl text-gray-700 mb-2 ${border ? 'border-b border-gray' : ''}`}>{title}</h2>}
     {children}
   </div>
 );
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,7 +9,7 @@ export default class Application extends App {
     return (
       <div className='flex flex-col flex-1 md:justify-center max-w-3xl mx-auto p-5 w-full'>
         <div>
-          <h1 className='text-6xl text-center text-gray-700'>personnummer</h1>
+          <h1 className='text-4xl md:text-6xl text-center text-gray-700'>personnummer</h1>
         </div>
         <Component {...pageProps} />
       </div>
