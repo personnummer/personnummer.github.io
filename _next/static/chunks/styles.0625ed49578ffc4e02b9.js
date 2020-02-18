3eefd434db2f9726c828457a467235706364c4d7
JiteBot <accounts+github@jitesoft.com>
2020-02-18 13:52:24 +0000

    Deploy(b93742d): Fix unmaintained format command
    Repo: personnummer/personnummer.github.io
    Workflow: .github/workflows/deploy.yml
    SHA: b93742dfc56ec369b142b16f3c60fd762edd3552
    Ref: refs/heads/develop
    Actor: rasmusbe

--- _next/static/chunks/styles.0625ed49578ffc4e02b9.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{fnl6:function(n,w,o){}}]);
---
+++ _next/static/chunks/styles.25d71142a727a55cdaf9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{fnl6:function(n,w,o){}}]);
