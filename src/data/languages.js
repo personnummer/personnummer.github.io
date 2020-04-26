f7dfdefc6e5922c1830befd6afffb9d6b1799e3e
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-04-26 20:59:17 +0200

    Add all langauges and more info about new languages

---
+++ src/data/languages.js
@@ -0,0 +1,70 @@
+export default [
+  {
+    name: 'Bash',
+    link: 'https://github.com/personnummer/bash',
+  },
+  {
+    name: 'C#',
+    link: 'https://github.com/personnummer/csharp',
+  },
+  {
+    name: 'D',
+    link: 'https://github.com/personnummer/d',
+  },
+  {
+    name: 'Dart',
+    link: 'https://github.com/personnummer/dart',
+  },
+  {
+    name: 'Elixir',
+    link: 'https://github.com/personnummer/ruby',
+  },
+  {
+    name: 'Go',
+    link: 'https://github.com/personnummer/go',
+  },
+  {
+    name: 'Java',
+    link: 'https://github.com/personnummer/java',
+  },
+  {
+    name: 'JavaScript',
+    link: 'https://github.com/personnummer/js',
+  },
+  {
+    name: 'Kotlin',
+    link: 'https://github.com/personnummer/kotlin',
+  },
+  {
+    name: 'Lua',
+    link: 'https://github.com/personnummer/lua',
+  },
+  {
+    name: 'Perl',
+    link: 'https://github.com/personnummer/perl',
+  },
+  {
+    name: 'PHP',
+    link: 'https://github.com/personnummer/php',
+  },
+  {
+    name: 'Python',
+    link: 'https://github.com/personnummer/python',
+  },
+  {
+    name: 'Ruby',
+    link: 'https://github.com/personnummer/ruby',
+  },
+  {
+    name: 'Rust',
+    link: 'https://github.com/personnummer/rust',
+  },
+  {
+    name: 'Scala',
+    link: 'https://github.com/personnummer/scala',
+  },
+  {
+    name: 'Swift',
+    link: 'https://github.com/personnummer/swift',
+  }
+];
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,136 +5,9 @@ import Implementations from '../components/Implementations';
 import Try from '../components/Try';
 import Generate from '../components/Generate';
 
-const languages = [
-  {
-    name: 'JavaScript',
-    hljs: 'js',
-    link: 'https://github.com/personnummer/js',
-    code: `const personnummer = require('personnummer');
-
-personnummer.valid('19121212+1212');`,
-    install: 'npm install --save personnummer'
-  },
-  {
-    name: 'Ruby',
-    hljs: 'ruby',
-    link: 'https://github.com/personnummer/ruby',
-    install:
-      "gem 'personnummer', :git => 'https://github.com/personnummer/ruby.git'",
-    code: `require 'personnummer'
-
-Personnummer::valid('19121212+1212')`
-  },
-  {
-    name: 'Python',
-    hljs: 'python',
-    link: 'https://github.com/personnummer/python',
-    install: `pip install personnummer
-
-// or
-
-pip3 install personnummer`,
-    code: `from personnummer import personnummer
-
-personnummer.valid('19121212+1212')`
-  },
-  {
-    name: 'PHP',
-    hljs: 'php',
-    link: 'https://github.com/personnummer/php',
-    install: 'composer require personnummer/personnummer',
-    code: `use Personnummer\\Personnummer;
-
-Personnummer::valid('19121212+1212');`
-  },
-  {
-    name: 'Dart',
-    hljs: 'dart',
-    link: 'https://github.com/personnummer/dart',
-    install: `1. Depend on it
-
-dependencies:
-  personnummer:
-
-2. Install it
-
-// with pub
-$ pub get
-
-// with flutter
-$ flutter pub get
-`,
-    code: `import 'package:personnummer/personnummer.dart';
-
-Personnummer.valid('19121212+1212');
-`
-  },
-  {
-    name: 'Go',
-    hljs: 'go',
-    link: 'https://github.com/personnummer/go',
-    install: 'go get -u github.com/personnummer/go',
-    code: `package main
-
-import (
-  personnummer "github.com/personnummer/go"
-)
-
-func main() {
-  personnummer.Valid("19121212+1212")
-}
-`
-  },
-  {
-    name: 'Java',
-    hljs: 'java',
-    link: 'https://github.com/personnummer/java',
-    install: '???',
-    code: `class Example {
-  public void main(String[] args) {
-    Personnummer.valid("19121212+1212");
-  }
-}`
-  },
-  {
-    name: 'C#',
-    hljs: 'csharp',
-    link: 'https://github.com/personnummer/csharp',
-    install: 'dotnet add package Personnummer',
-    code: `use Personnummer;
-
-class Example
-{
-  public Example()
-  {
-    Personnummer.Valid("19121212+1212")
-  }
-}
-`
-  },
-  {
-    name: 'Kotlin',
-    hljs: 'kotlin',
-    link: 'https://github.com/personnummer/kotlin',
-    install: '???',
-    code: `import personnummer.Personnummer
-
-fun main(args: Array<String>) {
-  Personnummer.valid("19121212+1212")
-}`
-  },
-  {
-    name: 'Swift',
-    hljs: 'swift',
-    link: 'https://github.com/personnummer/swift',
-    install: `1. Add dependency to Podfile
-pod 'Personnummer'
-
-2. Then run
-pod install`,
-    code: 'Personnummer.isValid("19121212+1212")'
-  }
-].sort((a, b) => a.name.localeCompare(b.name));
+const languages = require('../data/languages').default.sort((a, b) =>
+  a.name.localeCompare(b.name)
+);
 
 export default () => {
   const [language, setLanguage] = useState(languages[0]);
@@ -165,62 +38,25 @@ export default () => {
           </a>
           .
         </p>
-        <ul className="hidden md:flex list-reset border-b border-gray mt-3">
+        <p className="pt-2 text-center">
+          <span className="text-gray-500 pr-1">available in:</span>
           {languages.map((l, li) => (
-            <li
-              key={li}
-              className={`${l.name === language.name ? '-mb-px' : ''} mr-1`}
-            >
+            <>
+              {li + 1 === languages.length && (
+                <span className="text-gray-500 pr-1">and for</span>
+              )}
               <a
-                className={`bg-white inline-block py-2 px-4 text-blue-500 font-semibold cursor-pointer ${
-                  l.name === language.name
-                    ? 'border-l border-t border-r rounded-t'
-                    : 'hover:text-blue-600'
-                }`}
-                key={li}
-                onClick={() => setLanguage(l)}
+                className="inline-block text-blue-500 cursor-pointer hover:underline"
+                rel="noopener noreferrer"
+                target="_blank"
               >
                 {l.name}
               </a>
-            </li>
+              <span className="pr-1">
+                {li >= languages.length - 2 ? '' : ','}
+              </span>
+            </>
           ))}
-        </ul>
-        <p className="mt-3 block md:hidden">select language to show usage</p>
-        <select
-          name="languages"
-          id="languages"
-          value={language.name}
-          className="block md:hidden mt-3 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          onChange={e =>
-            setLanguage(languages.filter(l => l.name === e.target.value).pop())
-          }
-        >
-          {languages.map((l, li) => (
-            <option key={li} value={l.name}>
-              {l.name}
-            </option>
-          ))}
-        </select>
-      </Block>
-
-      <Block border={false}>
-        <p>installation</p>
-        <Highlight className="my-2" languages={['shell']}>
-          {language.install}
-        </Highlight>
-        <p className="pt-3">example</p>
-        <Highlight className="my-2" languages={[language.hljs]}>
-          {language.code}
-        </Highlight>
-        <p className="pt-3">
-          <a
-            target="_blank"
-            rel="noopener noreferrer"
-            href={language.link}
-            className="text-blue-500 hover:underline"
-          >
-            read more
-          </a>
         </p>
       </Block>
 
@@ -242,6 +78,61 @@ export default () => {
       <Generate title="generate personal identity numbers" />
 
       <Implementations title="implementations" />
+
+      <p className="pt-3">
+        some implementations may be work in progress and if you've found a bug,
+        please open a issue in the specific language repo.
+      </p>
+
+      <p className="pt-3">
+        if an implementation in your favorite language is missing, feel free to
+        port it over from one of the existing versions. There's still plenty of
+        languages to contribute in:{' '}
+        <a
+          target="_blank"
+          href="https://www.r-project.org/"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline"
+        >
+          R
+        </a>
+        ,{' '}
+        <a
+          target="_blank"
+          href="http://programming.nu/"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline"
+        >
+          Nu
+        </a>
+        ,{' '}
+        <a
+          target="_blank"
+          href="http://groovy-lang.org/"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline"
+        >
+          Groovy
+        </a>
+        ,{' '}
+        <a
+          target="_blank"
+          href="https://julialang.org/"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline"
+        >
+          Julia
+        </a>
+        ,{' '}
+        <a
+          target="_blank"
+          href="https://fsharp.org/"
+          rel="noopener noreferrer"
+          className="text-blue-500 hover:underline"
+        >
+          F#
+        </a>
+      </p>
 
       <Block title="license" className="mb-20">
         <p>
