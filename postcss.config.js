f32c9f52ef05839766e0296d6803985e509284e0
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-11-11 22:03:59 +0100

    Fix issue with updated autoprefixer and postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
 const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
 module.exports = {
   plugins: [
     require('postcss-easy-import'),
     tailwindcss('./tailwind.config.js'),
-    require('autoprefixer')
-  ]
+    autoprefixer(),
+  ],
 };
