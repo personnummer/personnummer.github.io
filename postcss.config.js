30a451fd34a4ed49e27144411541e82794614f8e
Johannes Tegnér <johannes@jitesoft.com>
2020-11-12 14:38:02 +0100

    Autoprefixer fix.
    Signed-off-by: Johannes Tegnér <johannes@jitesoft.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
 const tailwindcss = require('tailwindcss');
-const autoprefixer = require('autoprefixer');
+const autoprefixer = require('autoprefixer').default;
 module.exports = {
   plugins: [
     require('postcss-easy-import'),
     tailwindcss('./tailwind.config.js'),
-    autoprefixer(),
+    autoprefixer
   ],
 };
