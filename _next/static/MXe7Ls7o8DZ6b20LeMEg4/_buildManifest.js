4bcebe8bf3643e81538d4b31c7d8fc72cdde51d8
JiteBot <accounts+github@jitesoft.com>
2020-11-25 05:06:08 +0000

    Deploy(41c21f2): Merge pull request #35 from personnummer/dependabot/npm_and_yarn/next-10.0.3
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 41c21f23d5134e07671225d8032bcd746cbeb3c9
    Ref: refs/heads/develop
    Actor: dependabot[bot]

---
+++ _next/static/MXe7Ls7o8DZ6b20LeMEg4/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-8e3bea70f08222c6cc58.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e8770fdf1841c5084fab.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
