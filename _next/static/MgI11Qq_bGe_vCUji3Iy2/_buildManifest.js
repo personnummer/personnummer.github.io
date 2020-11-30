9bcdba833bc5ad58960c4fae4be80028f71db858
JiteBot <accounts+github@jitesoft.com>
2020-11-30 05:13:23 +0000

    Deploy(1890727): Merge pull request #37 from personnummer/dependabot/npm_and_yarn/autoprefixer-10.0.4
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 1890727c2c6a44b1f5f288f8a68c20855e56fb1e
    Ref: refs/heads/develop
    Actor: dependabot[bot]

--- _next/static/MgI11Qq_bGe_vCUji3Iy2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-94ab6e40f75296659906.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-e8770fdf1841c5084fab.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
