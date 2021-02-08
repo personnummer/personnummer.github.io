e6140d5c1bbf146f506a19f51dee86770f0417ba
JiteBot <accounts+github@jitesoft.com>
2021-02-08 06:40:16 +0000

    Deploy(8c1806f): Merge pull request #56 from personnummer/dependabot/npm_and_yarn/marked-2.0.0
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 8c1806fcedc885d0010a44b012d2535f15380458
    Ref: refs/heads/develop
    Actor: rasmusbe

--- _next/static/-Dbnueux8ZHTpUXaEbVGw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1ce8548b1e2037f7ab24.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b2e8d81a7624e665d650.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/XGR5qQxnz8x6XQbdO6LC2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-79023285062fd45462a1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-b2e8d81a7624e665d650.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
