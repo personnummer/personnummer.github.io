661bdb5ec5570e2b33d74d2b68ee115fec52158a
JiteBot <accounts+github@jitesoft.com>
2021-03-16 13:12:31 +0000

    Deploy(6f59aeb): Merge pull request #66 from personnummer/renovate/pin-dependencies
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 6f59aeb9baeb7118ab8d2285169951e3f27f703e
    Ref: refs/heads/develop
    Actor: rasmusbe

---
+++ _next/static/SEBn5gTH8fn8VoIgHHyqj/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-d229be8f1c079ea91a80.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9d1cac002dfa74d967f9.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
