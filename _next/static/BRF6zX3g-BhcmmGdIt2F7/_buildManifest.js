6afc700a83cf2fc5bf1ddffab87440e26075a684
JiteBot <accounts+github@jitesoft.com>
2021-03-16 13:55:38 +0000

    Deploy(958db68): Merge pull request #69 from personnummer/renovate/webpack-5.x
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 958db689757eafbe8aa1f87147ece14f42fcca72
    Ref: refs/heads/develop
    Actor: rasmusbe

--- _next/static/BRF6zX3g-BhcmmGdIt2F7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-8be74aebc31f68b486ff.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-9d1cac002dfa74d967f9.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/chunks/styles.10176661a556730c8b2e.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[8],{fnl6:function(n,w,o){}}]);
