1988a682502ccdcfd237c24f7e6f22d3534e0336
JiteBot <accounts+github@jitesoft.com>
2021-04-23 20:47:55 +0000

    Deploy(d288795): Bump dev dependencies
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: d288795ff4ef04c815eede46af11df059d7d2e7b
    Ref: refs/heads/develop
    Actor: frozzare

--- _next/static/hDJDe4CoXja0j7oEIoZzg/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/598-f3ed7ed57add2927bb54.js","static/chunks/pages/index-c340f452f3e286c4ed8f.js"],"/_error":["static/chunks/pages/_error-746228ba7d6c5d500d78.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
