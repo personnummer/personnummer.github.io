f54949353dfbd1fbf72097b5f94fe146cb7a8d8e
JiteBot <accounts+github@jitesoft.com>
2021-06-04 18:24:18 +0000

    Deploy(6f44ff3): Bump dependencies
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 6f44ff34d0abb6b4facfd7d49852cf66aa98f74c
    Ref: refs/heads/develop
    Actor: frozzare

---
+++ _next/static/KtbeNwo5-NU4STifKtLyo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/972-00d8bdba5b1965dd8563.js","static/chunks/pages/index-ca82522456f10147308f.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
