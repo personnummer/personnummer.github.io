8e3ead083685e6ee37c00ae850ecc5d94e139157
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-06-15 21:10:29 +0200

    Bump next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   trailingSlash: true,
-  future: {
-    webpack5: true
-  },
-  webpack: function (config, options) {
-    console.log(`Using Webpack ${options.webpack.version}`);
-    config.experiments = {};
-    return config;
-  }
 };
