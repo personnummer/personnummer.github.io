b1660a49a7aaa8b8e7c450059139711478a96ee5
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-09-09 19:25:13 +0200

    Fix table for mobile

--- scripts/implementations.js
+++ scripts/implementations.js
@@ -5,9 +5,13 @@ const path = require('path');
 const prettier = require('prettier');
 
 (async () => {
-  const res = await fetch('https://raw.githubusercontent.com/personnummer/meta/master/README.md');
+  const res = await fetch(
+    'https://raw.githubusercontent.com/personnummer/meta/master/README.md'
+  );
   const text = await res.text();
-  const html = marked(text).trim().replace(/(\r\n|\n|\r)/gm, '');
+  const html = marked(text)
+    .trim()
+    .replace(/(\r\n|\n|\r)/gm, '');
   const output = /<table>(.*?)<\/table>/.exec(html);
 
   if (!output) {
@@ -17,24 +21,44 @@ const prettier = require('prettier');
 
   let table = output[0];
 
-  table = table.replace(/<table>/g, '<table className="table-fixed w-full">');
+  table = table.replace(
+    /<table>/g,
+    '<table className="lg:table-fixed w-full">'
+  );
   table = table.replace(/<th>/g, '<th className="px-4 py-2 lowercase">');
   table = table.replace(/<td>/g, '<td className="border px-4 py-2">');
-  table = table.replace(/<a/g, '<a className="text-blue-500 hover:underline" rel="noopener noreferrer" ');
+  table = table.replace(
+    /<a/g,
+    '<a className="text-blue-500 hover:underline" rel="noopener noreferrer" '
+  );
   table = table.replace(/><\/a>/g, '/></a>');
 
   const template = `import Block from '../Block';
 
   const Implementations = (props) => (
     <Block {...props}>
-    ${table}
+      <div className="overflow-auto">
+        ${table}
+      </div>
     </Block>
   );
 
   export default Implementations;`;
 
-  prettier.resolveConfig(path.join(__dirname, '..', '.prettierrc')).then((options) => {
-    fs.writeFileSync(path.join(__dirname, '..', 'src', 'components', 'Implementations', 'index.js'), prettier.format(template, options));
-    console.log('Updated Implementations component');
-  });
+  prettier
+    .resolveConfig(path.join(__dirname, '..', '.prettierrc'))
+    .then((options) => {
+      fs.writeFileSync(
+        path.join(
+          __dirname,
+          '..',
+          'src',
+          'components',
+          'Implementations',
+          'index.js'
+        ),
+        prettier.format(template, options)
+      );
+      console.log('Updated Implementations component');
+    });
 })();
