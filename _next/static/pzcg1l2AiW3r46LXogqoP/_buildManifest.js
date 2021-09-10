7b6585776067d105146ee7110e850407d572ab60
JiteBot <accounts+github@jitesoft.com>
2021-09-10 12:04:34 +0000

    Deploy(548ec0a): Fix desktop table overflow
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 548ec0a26b49057f515285acb789ce6775adcdc6
    Ref: refs/heads/develop
    Actor: frozzare

--- _next/static/pzcg1l2AiW3r46LXogqoP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/972-b6849c0496d25870c7c4.js","static/chunks/pages/index-c654e62021d1833a3590.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
