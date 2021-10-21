d50a23382a5e9e53b176bcb16321b35d33523de5
JiteBot <accounts+github@jitesoft.com>
2021-10-21 15:09:33 +0000

    Deploy(cffd4d6): Remove next/image import
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: cffd4d60ea0ad964b505d9c7983f997395bd7603
    Ref: refs/heads/develop
    Actor: frozzare

--- _next/static/yTUS0GA9RP0ajohIsZ5Mu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/577-bc87bf0686364282bb03.js","static/chunks/pages/index-a1aaa3bede251d4bc065.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
