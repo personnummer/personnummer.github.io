bfd30d04ec6021152c928cc016f06a8f6dc6ed35
JiteBot <accounts+github@jitesoft.com>
2021-10-26 17:10:29 +0000

    Deploy(30f72ce): Upgrade to next 12
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 30f72ceebb84c75f8a3bbe48a43e3ab1df528001
    Ref: refs/heads/develop
    Actor: frozzare

---
+++ _next/static/Nq3l2bxkCdjUbvnDdf1bQ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/577-b7312cf6e9a8c62d.js","static/chunks/pages/index-4c5d83faac36502d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
