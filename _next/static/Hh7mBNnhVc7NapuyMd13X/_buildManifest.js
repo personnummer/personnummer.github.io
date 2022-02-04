edc2282039df0d21f8eb479d16ee921520fbe8f5
JiteBot <accounts+github@jitesoft.com>
2022-02-04 10:04:18 +0000

    Deploy(2f56079): Fix who is using this layout
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 2f560790c97c8c5eb9a9519b3676708fe71e5467
    Ref: refs/heads/develop
    Actor: frozzare

---
+++ _next/static/Hh7mBNnhVc7NapuyMd13X/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/577-9aec790c5c51c9ea.js","static/chunks/pages/index-a1b50a0404a6dd7f.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
