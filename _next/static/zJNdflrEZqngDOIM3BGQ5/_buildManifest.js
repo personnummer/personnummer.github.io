ff9b609626026951de2de849220e624c81e8efc9
JiteBot <accounts+github@jitesoft.com>
2022-02-11 02:10:37 +0000

    Deploy(d4430bf): Update dependency tailwindcss to v3.0.21
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: d4430bfd3a4a770d06c1f785610e7c48e33f65fc
    Ref: refs/heads/develop
    Actor: renovate[bot]

--- _next/static/zJNdflrEZqngDOIM3BGQ5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/577-9aec790c5c51c9ea.js","static/chunks/pages/index-69ac7c560b7b08eb.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
