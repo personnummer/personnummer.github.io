aa89dc4eccf602f07de5afe416c1fda41ad1d01f
JiteBot <accounts+github@jitesoft.com>
2022-03-31 10:21:19 +0000

    Deploy(ca3509d): Merge branch 'develop' of github.com:personnummer/personnummer.github.io into develop
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: ca3509dba90da0d9feab13d22013eb3e3d9ea5a6
    Ref: refs/heads/develop
    Actor: frozzare

---
+++ _next/static/trbB0QkcFRBXbsgFxOYEy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-0e147e65df394a3f.js","static/chunks/pages/index-92bb27967d441608.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
