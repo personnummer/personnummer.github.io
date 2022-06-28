a0a646210b732c9b98139fca1cead8c4a48f2dde
JiteBot <accounts+github@jitesoft.com>
2022-06-28 19:09:38 +0000

    Deploy(01d097a): Update nextjs monorepo to v12.2.0
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 01d097adeb57d5c6dd0adf9676f7cb65a8f7db43
    Ref: refs/heads/develop
    Actor: renovate[bot]

--- _next/static/5zUwHlJHdefjkMzRg6cVO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-cd3f74e09b13782a.js","static/chunks/pages/index-29434392123191c3.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
