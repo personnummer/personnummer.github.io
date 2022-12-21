ac7fdd7eb5948ef3ffc2fc78d4378cdbb561f0dd
JiteBot <accounts+github@jitesoft.com>
2022-12-21 10:19:56 +0000

    Deploy(bbd9a55): Trigger Build
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: bbd9a55e4cba0c98ec80cc30ae26d929bbd5d1ca
    Ref: refs/heads/develop
    Actor: frozzare

--- _next/static/EVTgA8cn00JMSNQfPOHQv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/905-79cc7603a604ebce.js","static/chunks/pages/index-f3d2cd5ab0a60ddb.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
