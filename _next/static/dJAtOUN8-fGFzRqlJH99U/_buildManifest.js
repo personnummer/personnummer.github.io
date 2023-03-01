cb11fece61f7cd68a0588916c5dbcc8ab3f7d991
JiteBot <accounts+github@jitesoft.com>
2023-03-01 08:03:23 +0000

    Deploy(8233e63): Merge pull request #440 from personnummer/renovate/nextjs-monorepo
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 8233e631a53e359821388933c15a5c5c6a264eb8
    Ref: refs/heads/develop
    Actor: frozzare

--- _next/static/dJAtOUN8-fGFzRqlJH99U/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/512-08a7f06a743dbd2a.js","static/chunks/pages/index-fdbee73e296a73fd.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
