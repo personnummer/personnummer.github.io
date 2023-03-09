febf2d35b4f1dba5f59647fd0c6caaadc4ef6109
JiteBot <accounts+github@jitesoft.com>
2023-03-09 08:51:32 +0000

    Deploy(1969ff3): Add interim to personnummer object
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 1969ff38525dcac2766066406224d9c547edab2a
    Ref: refs/heads/develop
    Actor: frozzare

--- _next/static/JCVSeVtSo3FqAcFxMOwow/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/512-08a7f06a743dbd2a.js","static/chunks/pages/index-6f5a1ff2ccafb08f.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/Q-91HlyLWpLEZgOUYy5ZI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/512-3a32319894a06d3a.js","static/chunks/pages/index-f80905e357e462a2.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
