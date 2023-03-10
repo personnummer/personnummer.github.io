7ffeabf5bce0fe1e0e55fddcb73141fb73cc111c
JiteBot <accounts+github@jitesoft.com>
2023-03-10 05:49:52 +0000

    Deploy(0cf7761): Update nextjs monorepo to v13.2.4
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: 0cf77610b9ed3f5ce6ad99ac323fde87890544de
    Ref: refs/heads/develop
    Actor: renovate[bot]

---
+++ _next/static/LUDwy_ytnlwqRG0-_97S6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/512-3a32319894a06d3a.js","static/chunks/pages/index-765832b09eae917d.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
