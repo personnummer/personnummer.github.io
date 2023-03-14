bcf709f95d9e4b0116453146e3c9071766c9836c
JiteBot <accounts+github@jitesoft.com>
2023-03-14 10:09:04 +0000

    Deploy(aa70077): Add interim number feature
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: aa700771e9d666f1a42613607518679ddfcf94d4
    Ref: refs/heads/develop
    Actor: frozzare

---
+++ _next/static/DH5WO2aVJpO0yxPgTfdu5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/512-3a32319894a06d3a.js","static/chunks/pages/index-b51ff73664b05fb2.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
