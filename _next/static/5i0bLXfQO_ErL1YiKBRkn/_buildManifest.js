51a8ddd8e19adc0a2030b2dedc97e2158cd62964
JiteBot <accounts+github@jitesoft.com>
2023-03-14 02:38:07 +0000

    Deploy(ce76d3c): Update dependency @types/node to v18.15.2
    Repo: personnummer/personnummer.github.io
    Workflow: Install and build
    SHA: ce76d3c00c7f39140f9bbc33d9fb103f9d3f8296
    Ref: refs/heads/develop
    Actor: renovate[bot]

---
+++ _next/static/5i0bLXfQO_ErL1YiKBRkn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/512-3a32319894a06d3a.js","static/chunks/pages/index-9b5c57765c60d531.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
