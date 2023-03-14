aa700771e9d666f1a42613607518679ddfcf94d4
Fredrik Forsmo <frozzare@users.noreply.github.com>
2023-03-14 11:08:16 +0100

    Add interim number feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -66,6 +66,7 @@ const Index = () => (
         <li>determine age</li>
         <li>determine sex</li>
         <li>determine if coordination number</li>
+        <li>determine if interim number</li>
       </ul>
       <p className="pt-3">
         All features might not be implemented in all languages. Full
