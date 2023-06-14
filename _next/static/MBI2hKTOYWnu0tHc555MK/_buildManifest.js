4610a8421f311fdd5844c2eb1b5636eefa805d48
JiteBot <accounts+github@jitesoft.com>
2023-06-14 18:20:07 +0000

    Deploy(51d15d3): Add missing dots
    Repo: personnummer/personnummer.github.io
    Workflow: Deploy
    SHA: 51d15d3d749d9324dc2e0a49f49348c98e4189a8
    Ref: refs/heads/develop
    Actor: frozzare

---
+++ _next/static/MBI2hKTOYWnu0tHc555MK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/512-bb665818656e1ecc.js","static/chunks/pages/index-9443b88ce2754cfe.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
