c4b01b6454354e281d4565b29ccc8cbe155644c6
JiteBot <accounts+github@jitesoft.com>
2023-10-16 18:04:58 +0000

    Deploy(4e3e0fb): Update nextjs monorepo to v13.5.5
    Repo: personnummer/personnummer.github.io
    Workflow: Deploy
    SHA: 4e3e0fb2e10d59f1dd175ccbadd81da9a2612580
    Ref: refs/heads/develop
    Actor: renovate[bot]

--- _next/static/Aqby6HwQnDD7cvd9SZNnD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/512-f7b5227b3533552e.js","static/chunks/pages/index-0c81af60fd4dd79b.js"],"/_error":["static/chunks/pages/_error-08a9db0f433628d8.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
