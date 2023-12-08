d2ae5e548658ac86bb906955f8796e0759954607
JiteBot <accounts+github@jitesoft.com>
2023-12-08 00:48:18 +0000

    Deploy(a9cbc26): Update nextjs monorepo to v14.0.4
    Repo: personnummer/personnummer.github.io
    Workflow: Deploy
    SHA: a9cbc26543e6914e8444978b36b634f5ca833371
    Ref: refs/heads/develop
    Actor: renovate[bot]

---
+++ _next/static/f7tW0i9XuJpKthYYZ6CzZ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/512-b5a03370f4f6199d.js","static/chunks/pages/index-9dce892f6c2fc9cc.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
