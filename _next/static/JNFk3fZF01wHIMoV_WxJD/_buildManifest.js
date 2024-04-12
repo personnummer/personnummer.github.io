8a54138b7db2b73e09d3d9bfe9db53351b4584bd
JiteBot <accounts+github@jitesoft.com>
2024-04-12 04:26:21 +0000

    Deploy(51fb3c1): Update nextjs monorepo to v14.2.0
    Repo: personnummer/personnummer.github.io
    Workflow: Deploy
    SHA: 51fb3c1aa4a2a3393f4218887aea2c302b7a68eb
    Ref: refs/heads/develop
    Actor: renovate[bot]

--- _next/static/JNFk3fZF01wHIMoV_WxJD/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/512-b5a03370f4f6199d.js","static/chunks/pages/index-cbf6f2379dc2f6b6.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/xhbpdQ3NUVrAPH5a4JvNy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/512-b5a03370f4f6199d.js","static/chunks/pages/index-168abc6cb1c9520c.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
