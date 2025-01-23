6718c3bbc5ce948d5becf979bbc488be7f331508
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2025-01-23 19:40:40 +0100

    Upgrade to tailwind v4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
-import 'tailwindcss/tailwind.css';
 import '../styles/main.css';
 
 const App = ({ Component, pageProps }: AppProps) => (
