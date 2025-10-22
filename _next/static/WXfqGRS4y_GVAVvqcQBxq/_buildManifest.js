c5716c1c41b916329829ba46d82704222bf00e1c
JiteBot <accounts+github@jitesoft.com>
2025-10-22 12:35:21 +0000

    Deploy(7fe0749): Bump to next 16
    Repo: personnummer/personnummer.github.io
    Workflow: Deploy
    SHA: 7fe074918d6824310e407d744e55898bfa43a82b
    Ref: refs/heads/develop
    Actor: frozzare

---
+++ _next/static/WXfqGRS4y_GVAVvqcQBxq/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/79278f223e1d6221.js"
+  ],
+  "/_error": [
+    "static/chunks/62fd98893a170a22.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/chunks/62fd98893a170a22.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/3c11a788f7bb0c92.js",
+  "static/chunks/01c9b21a9f34a83f.js",
+  "static/chunks/turbopack-722c75fa4d0fbc66.js"
+])
