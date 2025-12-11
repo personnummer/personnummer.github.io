e7419691da04d483047444cdb1fcc20988d71a80
JiteBot <accounts+github@jitesoft.com>
2025-12-11 01:15:41 +0000

    Deploy(09c6670): Update Node.js to v24.12.0 (#1250)
    Repo: personnummer/personnummer.github.io
    Workflow: Deploy
    SHA: 09c66702b6d78f29b64f3e125614ed4b623a5fd3
    Ref: refs/heads/develop
    Actor: renovate[bot]

--- _next/static/GS-lTEgS1tRhDXnT_Sm-q/_buildManifest.js
+++ _next/static/GS-lTEgS1tRhDXnT_Sm-q/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/dfcc818e8d19a337.js"
+    "static/chunks/43d1911bf7b642bf.js"
   ],
   "/_error": [
     "static/chunks/b8c650128ce39ab3.js"
--- _next/static/chunks/dfcc818e8d19a337.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/033417e7805c8b47.js",
-  "static/chunks/71dd04de67d0a1be.js",
-  "static/chunks/turbopack-0928bc9507a58431.js"
-])
