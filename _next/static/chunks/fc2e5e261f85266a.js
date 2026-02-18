276126e47a9205c942155fef77b75e90f0510a63
JiteBot <accounts+github@jitesoft.com>
2026-02-18 21:01:21 +0000

    Deploy(4506441): chore(deps): update tailwindcss monorepo to v4.2.0 (#1289)
    Repo: personnummer/personnummer.github.io
    Workflow: Deploy
    SHA: 45064418695bd28b9f71e850396cb29eec3d1018
    Ref: refs/heads/develop
    Actor: renovate[bot]

--- _next/static/chunks/fc2e5e261f85266a.js
+++ _next/static/chunks/fc2e5e261f85266a.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/_app", [
   "static/chunks/48a52f504908dd65.js",
   "static/chunks/8d95a0aef7635018.js",
-  "static/chunks/e4d92218d09cc234.css",
-  "static/chunks/turbopack-28f8c07870ba5a2b.js"
+  "static/chunks/e4aac28dff012cc0.css",
+  "static/chunks/turbopack-95071a77e9653c8a.js"
 ])
