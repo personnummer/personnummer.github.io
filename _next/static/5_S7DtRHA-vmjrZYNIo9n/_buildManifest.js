5bd10d0b1f862785b54593e52eca2f8b38d7285c
JiteBot <accounts+github@jitesoft.com>
2026-06-28 21:34:27 +0000

    Deploy(87ea69f): chore(deps): update dependency postcss to v8.5.16 (#1368)
    Repo: personnummer/personnummer.github.io
    Workflow: Deploy
    SHA: 87ea69f9c8630855c3ee5bb3dc3ec40bc67f0fbe
    Ref: refs/heads/develop
    Actor: renovate[bot]

--- _next/static/5_S7DtRHA-vmjrZYNIo9n/_buildManifest.js
+++ _next/static/5_S7DtRHA-vmjrZYNIo9n/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/1ivpmvictpw5z.js"
+    "static/chunks/2oe3fbmwxcxio.js"
   ],
   "/_error": [
     "static/chunks/2gvr1ru0n8xsz.js"
--- _next/static/chunks/2oe3fbmwxcxio.js
+++ _next/static/chunks/2oe3fbmwxcxio.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/", [
   "static/chunks/2wys4wmo0w9b9.js",
   "static/chunks/1-jzfvx57gyb-.js",
-  "static/chunks/0_chphmjz9aa0.js",
-  "static/chunks/turbopack-3axy1ccxre9ug.js"
+  "static/chunks/16ljk6f8oyb1f.js",
+  "static/chunks/turbopack-05yp6s3n61wwd.js"
 ])
