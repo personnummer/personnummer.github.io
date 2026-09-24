057d96670e107f8bef6ec28087319fbf6b53234e
JiteBot <accounts+github@jitesoft.com>
2026-09-24 03:06:40 +0000

    Deploy(abcc479): chore(deps): update dependency prettier to v3.9.9 (#1425)
    Repo: personnummer/personnummer.github.io
    Workflow: Deploy
    SHA: abcc479f841c02f5bb706fc54cbf1035911dd4b8
    Ref: refs/heads/develop
    Actor: renovate[bot]

--- _next/static/E7WSI9JVJ1fWG-aSJHSbI/_buildManifest.js
+++ _next/static/E7WSI9JVJ1fWG-aSJHSbI/_buildManifest.js
@@ -1,6 +1,6 @@
 self.__BUILD_MANIFEST = {
   "/": [
-    "static/chunks/1z41_jqstqju8.js"
+    "static/chunks/3qhazciyw-0tn.js"
   ],
   "/_error": [
     "static/chunks/1nsijy-bu0ij7.js"
--- _next/static/chunks/3qhazciyw-0tn.js
+++ _next/static/chunks/3qhazciyw-0tn.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/", [
   "static/chunks/43cvas0m_vwfw.js",
   "static/chunks/39j6uux2ryau8.js",
-  "static/chunks/3imy7idt44rfq.js",
-  "static/chunks/turbopack-0vi33tec1os4p.js"
+  "static/chunks/00jt4z61l7sc9.js",
+  "static/chunks/turbopack-40h3elcr5pmvy.js"
 ])
